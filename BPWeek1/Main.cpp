59b47a51ded667bd9c99bf7cc73c7556e8e82b7d
Metin Aslan <Metinaslan0547@gmail.com>
2022-09-07 13:16:15 +0200

    eerste les probeersels

---
+++ BPWeek1/Main.cpp
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+
+int main(void) {
+	
+
+	int a;
+	int b;
+	int c;
+
+	a = 10;
+	 
+	printf("remainder of %d divided by %d is %d\n", x, y, x % y);
+
+	 printf("voer een heel getal in..");
+	 scanf_s("%d", &b);
+	c = a * b;
+
+	if (b > 9) {
+		printf("het ingevoerde getal was groter dan 9\n");
+
+
+
+	else
+	printf("het ingevoerde getal kleiner of == aan 9\n"); 
+	}
+	 printf("Programmeren is leuk %\n", a);
+	 printf("A keer B is: %d", c);
+	return 0;
+}
